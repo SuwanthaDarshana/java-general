f71db6b6d42fe0547344da909bd1f7387b8642ac
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-15 12:36:59 +0530

    Add METHOD

---
+++ src/JavaTest/general/Method/CalculateSum.java
@@ -0,0 +1,29 @@
+package JavaTest.general.Method;
+
+import java.util.Scanner;
+
+public class CalculateSum {
+    public static void main(String[] args) {
+
+        System.out.println("Result is : "+ addSum(2,3));
+
+        multiply();
+
+    }
+
+    public static int addSum(int num1, int num2){
+        int sum = 0;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public static void multiply(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num3 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int num4 = scanner.nextInt();
+        int product = num3 * num4;
+        System.out.println("Product is: " + product);
+    }
+}
