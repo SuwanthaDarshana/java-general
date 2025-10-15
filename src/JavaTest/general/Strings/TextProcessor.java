0e14c2b1d9bf9e57f09de215420d74593a05e78e
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-15 23:29:52 +0530

    update String class with reverse type

--- src/JavaTest/general/Strings/TextProcessor.java
+++ src/JavaTest/general/Strings/TextProcessor.java
@@ -4,6 +4,7 @@ public class TextProcessor {
     public static void main(String[] args) {
 
         countWords("Hello my name is John");
+        reverseString("Hello");
 
     }
 
@@ -14,8 +15,15 @@ public class TextProcessor {
         String message = String.format("Number of words: %d", numberOfWords);
         System.out.println("Number of words: " + numberOfWords);
         for (int i = 0; i < numberOfWords; i++) {
-            System.out.println("Word " + i + ": " + words[i]);
+            System.out.println("Word " + (i+1) + ": " + words[i]);
         }
+    }
+
+    public static void reverseString(String text) {
+        for (int i = text.length()-1; i >= 0; i--) {
+            System.out.print(text.charAt(i));
+        }
+
     }
 
 }
