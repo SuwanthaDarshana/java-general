0f56dfe29fc805753285f013d769ed82fc74b06e
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-16 14:17:40 +0530

    add Class Object to oop

---
+++ src/JavaTest/oop/ClassObject/Car.java
@@ -0,0 +1,26 @@
+package JavaTest.oop.ClassObject;
+
+public class Car {
+
+    String brand;
+    int year;
+    String color;
+
+    Car(String brandName, int yearMade, String colorBody) {
+        brand = brandName;
+        year = yearMade;
+        color = colorBody;
+
+        //If we used same name for varibles in parameter use "this" keyword to refer to the instance variables
+       //If parameter names are the same as the class variables, you must use this to distinguish them.
+
+    }
+
+    void displayDetails(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Year: " + year);
+        System.out.println("Color: " + color);
+    }
+
+}
+
---
+++ src/JavaTest/oop/ClassObject/Main.java
@@ -0,0 +1,9 @@
+package JavaTest.oop.ClassObject;
+
+public class  Main {
+    public static void main(String[] args) {
+
+        Car car = new Car("Toyota", 2022, "Red");
+        car.displayDetails();
+    }
+}
