4873b0f52ec869b973d473c84d68421e26800fc1
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-20 20:41:53 +0530

    add files

---
+++ src/JavaTest/oop/Method/Marks.java
@@ -0,0 +1,15 @@
+package JavaTest.oop.Method;
+
+public class Marks {
+
+    int physics;
+    int chemistry;
+    int maths;
+
+    public void showMarks(){
+        System.out.println("Physics: " + physics);
+        System.out.println("Chemistry: " + chemistry);
+        System.out.println("Maths: " + maths);
+    }
+
+}
---
+++ src/JavaTest/oop/Method/Student.java
@@ -0,0 +1,11 @@
+package JavaTest.oop.Method;
+
+public class Student {
+    public static void main(String[] args) {
+        Marks  marks = new Marks();
+        marks.physics = 90;
+        marks.chemistry = 80;
+        marks.maths = 70;
+        marks.showMarks();
+    }
+}
