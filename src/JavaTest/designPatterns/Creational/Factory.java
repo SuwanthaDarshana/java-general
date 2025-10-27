d617c8263c5e04fdb36c73d9f9a68fde34579c6c
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-27 11:15:00 +0530

    update singleton

---
+++ src/JavaTest/designPatterns/Creational/Factory.java
@@ -0,0 +1,4 @@
+package JavaTest.designPatterns.Creational;
+
+public class Factory {
+}
--- src/JavaTest/designPatterns/Creational/Singleton.java
+++ src/JavaTest/designPatterns/Creational/Singleton.java
@@ -1,27 +1,27 @@
 package JavaTest.designPatterns.Creational;
 
 public class Singleton {
-    private static Singleton instance;
 
     private Singleton() {
-
     }
+
+    private static Singleton instance = new Singleton();
 
     public static Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton();
-        }
         return instance;
     }
-
 
     public void doSomething() {
         System.out.println("Singleton instance is doing something");
     }
 
+
+
+
     public static void main(String[] args) {
         Singleton singleton = Singleton.getInstance();
         singleton.doSomething();
+
     }
 
 }
