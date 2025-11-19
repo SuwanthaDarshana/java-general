60ccc93e50f5cc697a6a1af378d0b1e4b08e9012
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-19 23:58:13 +0530

    add abstract phone example

---
+++ src/JavaTest/general/Abstract/Example.java
@@ -0,0 +1,26 @@
+package JavaTest.general.Abstract;
+
+class SamanPhone{
+    void call(){
+        System.out.println("calling");
+    }
+
+    void message(){
+    }
+}
+
+class NimalPhone{
+    void call(){
+        System.out.println("calling");
+    }
+
+    void message(){
+    }
+}
+
+
+public class Example {
+    public static void main(String[] args) {
+
+    }
+}
