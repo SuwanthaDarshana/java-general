bf8c79848e4b4648b316c7c88c6c4090bf22d87e
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-20 23:42:34 +0530

    add linkedlist

---
+++ src/JavaTest/general/Collectionframework/list_s/Linkedlist.java
@@ -0,0 +1,16 @@
+package JavaTest.general.Collectionframework.list_s;
+
+import java.util.LinkedList;
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("java");
+        linkedList.add("python");
+        linkedList.add("c++");
+        linkedList.add("javascript");
+        linkedList.add("c");
+        linkedList.add("c#");
+        System.out.println(linkedList);
+    }
+}
