ad865a2df5788c750ae6511d947316edf34e3cd4
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-20 23:32:07 +0530

    add arraylist

--- src/JavaTest/general/Collectionframework/list_s/Example.java
+++ src/JavaTest/general/Collectionframework/list_s/Example.java
@@ -5,7 +5,31 @@ import java.util.List;
 
 public class Example {
     public static void main(String[] args) {
-        List list = new ArrayList();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("java");
+        list.add("python");
+        list.add("c++");
+        list.add("javascript");
+        list.add("c");
+        list.add("c#");
+        System.out.println(list);
+        System.out.println("-----------------");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println("-----------------");
+
+        list.remove("c");
+        System.out.println(list);
+
+        System.out.println("-----------------");
+
+        for (String temp : list) {
+            System.out.println(temp);
+        }
+        System.out.println("-----------------");
+
 
     }
 }
