97562c7281762f8870c22e5fdd30efcb5a92e00a
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-21 23:52:11 +0530

    add questions

---
+++ src/JavaTest/questions/q1/Example1.java
@@ -0,0 +1,26 @@
+package JavaTest.questions.q1;
+
+
+//Given an array of integers and a target number (e.g., 8), find two numbers in the array that add up to the target.
+
+public class Example1 {
+
+    public static int[] findTwoNumbers(int[],target)
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 12;
+
+        for(int i = 0; i<arr.length; i++){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println("The tow numbers that equel to target value are:  " + arr[i] + " "+ arr[j]);
+                }
+            }
+        }
+        System.out.println("No tow numbers that equel to target value");
+
+    }
+
+
+}
