82ef329c9095dd755a7b69516a309de3ee2b1e31
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-22 23:30:49 +0530

    add method

--- src/JavaTest/questions/q1/Example1.java
+++ src/JavaTest/questions/q1/Example1.java
@@ -5,20 +5,22 @@ package JavaTest.questions.q1;
 
 public class Example1 {
 
-    public static int[] findTwoNumbers(int[],target)
+    public static int[] findTwoNumbers(int[],target[]){
+
+    }
 
     public static void main(String[] args) {
-        int[] arr = {2, 7, 11, 15};
-        int target = 12;
-
-        for(int i = 0; i<arr.length; i++){
-            for(int j = i+1; j<arr.length; j++){
-                if(arr[i]+arr[j]==target){
-                    System.out.println("The tow numbers that equel to target value are:  " + arr[i] + " "+ arr[j]);
-                }
-            }
-        }
-        System.out.println("No tow numbers that equel to target value");
+//        int[] arr = {2, 7, 11, 15};
+//        int target = 12;
+//
+//        for(int i = 0; i<arr.length; i++){
+//            for(int j = i+1; j<arr.length; j++){
+//                if(arr[i]+arr[j]==target){
+//                    System.out.println("The tow numbers that equel to target value are:  " + arr[i] + " "+ arr[j]);
+//                }
+//            }
+//        }
+//        System.out.println("No tow numbers that equel to target value");
 
     }
 
