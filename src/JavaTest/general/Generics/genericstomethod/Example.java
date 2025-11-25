c117f998e5f6253c25f545aac212948718a62e77
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-26 00:33:40 +0530

    update generics to method

--- src/JavaTest/general/Generics/genericstomethod/Example.java
+++ src/JavaTest/general/Generics/genericstomethod/Example.java
@@ -1,4 +1,22 @@
 package JavaTest.general.Generics.genericstomethod;
 
+class PrintUtil{
+    public static <T> void printArray(T[] array){
+        for(T num : array){
+            System.out.print(num+ ", ");
+        }
+        System.out.println();
+    }
+
+}
+
+
 public class Example {
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        String[] strArray = {"A", "B", "C", "D", "E"};
+
+        PrintUtil.printArray(intArray);
+        PrintUtil.printArray(strArray);
+    }
 }
