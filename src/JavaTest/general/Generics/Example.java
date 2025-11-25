d2125e6ceb3fc05ec5b4a654c74fb20a36040249
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-11-25 22:41:13 +0530

    add generics

--- src/JavaTest/general/Generics/Example.java
+++ src/JavaTest/general/Generics/Example.java
@@ -1,7 +1,30 @@
 package JavaTest.general.Generics;
 
+import java.util.ArrayList;
+
+
+class OlBox<T>{
+    private T item;
+    public void setItem(T item){
+        this.item = item;
+    }
+    public T getItem(){
+        return this.item;
+    }
+}
+
 public class Example {
     public static void main(String[] args) {
+        OlBox<String> stringOlBox = new OlBox();
+        stringOlBox.setItem("Java");
 
+        OlBox<Integer> integerOlBox = new OlBox();
+        integerOlBox.setItem(150);
+
+        String str1 = (String) stringOlBox.getItem();
+        Integer str2 = (Integer) integerOlBox.getItem();
+
+        System.out.println(str1);
+        System.out.println(str2);
     }
 }
