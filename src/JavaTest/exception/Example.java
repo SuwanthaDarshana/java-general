90ae14b357ac8a52283327d600cd8c4834156952
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-23 21:16:16 +0530

    add try catch

--- src/JavaTest/exception/Example.java
+++ src/JavaTest/exception/Example.java
@@ -1,4 +1,16 @@
 package JavaTest.exception;
 
 public class Example {
+    public static void main(String[] args) {
+        int num = 0;
+        try {
+            int result = 10/num;
+            System.out.println(result);
+        }catch (ArithmeticException e){
+            e.printStackTrace();
+        }
+        System.out.println("End of the program");
+
+
+    }
 }
