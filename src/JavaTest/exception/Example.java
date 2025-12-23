2cf9cda18a5d638d704660b52079d1c695668d91
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-24 02:11:32 +0530

    add custom exception

--- src/JavaTest/exception/Example.java
+++ src/JavaTest/exception/Example.java
@@ -1,21 +1,38 @@
 package JavaTest.exception;
 
 public class Example {
-    public static void main(String[] args) {
+
+    public static void m1() throws RuntimeException {
+        System.out.println("M1 Start");
         int num = 0;
+        int result = 10 / num;
+        System.out.println(result);
+        System.out.println("M1 End");
+    }
+
+    public static void m2() throws ArithmeticException{
+        System.out.println("M2 Start");
+        m1();
+        System.out.println("M2 End");
+    }
+
+    public static void m3() throws ArithmeticException {
+        System.out.println("M3 Start");
+        m2();
+        System.out.println("M3 End");
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Start Main");
         try {
-            int result = 10/num;
-            System.out.println(result);
-            int[] arr = new int[5];
-            arr[20] = 10;
-        }catch (NullPointerException | ArithmeticException e){
+            m3();
+        }catch (Exception e){
             System.out.println(e.getMessage());
             e.printStackTrace();
-        }finally {
-            System.out.println("Finally run");
         }
-        System.out.println("End of the program");
-
+        System.out.println("End Main");
 
     }
 }
---
+++ src/JavaTest/exception/MyException.java
@@ -0,0 +1,7 @@
+package JavaTest.exception;
+
+public class MyException extends RuntimeException {
+    MyException(String message){
+        super(message);
+    }
+}
