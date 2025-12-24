80a2a21bbe656f1b21c877ba8759b7ec3862d681
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-24 23:42:31 +0530

    add String pool

--- src/JavaTest/general/stringApi/Example.java
+++ src/JavaTest/general/stringApi/Example.java
@@ -25,5 +25,14 @@ public class Example {
 
         System.out.println("---------------------------------");
 
+        String s4 = "abc";
+        String s5 = new String("abc");
+        String s6 = "abc";
+        String s7 = new String("abc");
+
+        System.out.println(s4 == s5);   //false
+        System.out.println(s4 == s6);   //true
+        System.out.println(s5 == s7);   //false
+
     }
 }
