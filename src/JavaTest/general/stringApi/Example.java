aaae56e2e7456a2965b17d3c2aae54acd6f041fd
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-25 17:50:25 +0530

    Update StringApi

--- src/JavaTest/general/stringApi/Example.java
+++ src/JavaTest/general/stringApi/Example.java
@@ -34,5 +34,33 @@ public class Example {
         System.out.println(s4 == s6);   //true
         System.out.println(s5 == s7);   //false
 
+        System.out.println("---------------------------------");
+
+        String name = "Nuwara";
+        String city = "Nuwara";
+
+        System.out.println("Name :" + name);
+        System.out.println("City :" + city);
+
+        name = name.concat( " Maya");
+        System.out.println(name.concat(" Guwa"));
+
+        System.out.println("Name :" + name);
+        System.out.println("City :" + city);
+
+        System.out.println("---------------------------------");
+
+        String text = "0123456789";
+        System.out.println(text.charAt(2)); //2
+        System.out.println(text.substring(2)); //23456789
+        System.out.println(text.substring(2,5)); //234
+
+
+        System.out.println("----------");
+        String test = " abc ";
+        System.out.println(test.length());   //5
+        test = test.trim();
+        System.out.println(test.length());   //3
+
     }
 }
