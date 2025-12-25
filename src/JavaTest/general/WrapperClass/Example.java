022d1fd353a54d7238537d885e236f0b5cb268f1
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-25 17:55:34 +0530

    update wrapper class

--- src/JavaTest/general/WrapperClass/Example.java
+++ src/JavaTest/general/WrapperClass/Example.java
@@ -1,4 +1,18 @@
 package JavaTest.general.WrapperClass;
 
 public class Example {
+
+    public static void main(String[] args) {
+        int x =10;
+        Integer y = x;   //autoboxing
+
+        Integer z = 20;
+        int w = z;       //unboxing
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(w);
+    }
+
 }
