06c188dc93567652ad701931115c1120ae718c1f
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-25 21:11:09 +0530

    add IO create file

--- src/JavaTest/InputOutput/Example.java
+++ src/JavaTest/InputOutput/Example.java
@@ -1,4 +1,21 @@
 package JavaTest.InputOutput;
 
+import java.io.File;
+
 public class Example {
+    public static void main(String[] args) {
+        File f1 = new File("com");
+        System.out.println("is exist:"+f1.exists());
+        boolean isCreated = f1.mkdir();
+        System.out.println("is created:"+isCreated);
+        System.out.println("is exist:"+f1.exists());
+
+
+//        System.out.println("is a file:" + f1.isFile());
+//        System.out.println("is a directory:" + f1.isDirectory());
+//
+//        System.out.println("is hidden:"+f1.isHidden());
+//        System.out.println("length:"+f1.length());
+
+    }
 }
